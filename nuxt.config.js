6c584ea9eac3696b282d2a7175b9437bbc265eb1
Olha Hai-Nyzhnyk <olha.hn@techcore.holdings>
2021-06-15 11:07:06 +0300

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -142,7 +142,7 @@ export default {
   // See https://strapi.nuxtjs.org/
   strapi: {
     entities: [{ name: 'articles' }, { name: 'categories' }, { name: 'home' }],
-    url: process.env.API_URL || 'http://localhost:1337',
+    url: process.env.API_URL,
   },
 
   // See https://sitemap.nuxtjs.org/
