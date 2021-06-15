8ba8d40d188d293dbbca85c82b46b0349b2e0029
Olha Hai-Nyzhnyk <olha.hn@techcore.holdings>
2021-06-15 11:32:41 +0300

    img uri

--- utils/medias.js
+++ utils/medias.js
@@ -2,7 +2,7 @@ export function getStrapiMedia(url) {
   // Check if URL is a local path
   if (url.startsWith('/')) {
     // Prepend Strapi address
-    return `${process.env.strapiBaseUri}${url}`;
+    return `${'https://admin.fairspin.info'}${url}`;
   }
   // Otherwise return full URL
   return url;
