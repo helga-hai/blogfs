0bb1fb65df975e842c6618f796cf3b14454932ef
Olha Hai-Nyzhnyk <olha.hn@techcore.holdings>
2021-06-26 12:01:15 +0300

    font font

--- plugins/webFontLoader.js
+++ plugins/webFontLoader.js
@@ -1,8 +1,11 @@
 import WebFont from 'webfontloader';
 
 WebFont.load({
+  // gogle: {
+  //   families: ['Fira Sans:400,500,600,700:latin,vietnamese']
+  // }o
   custom: {
     families: ['Stolzl'],
-    urls: ['/font.css'],
+    urls: ['https://fairspin.io/static/font.css'],
   },
 });
